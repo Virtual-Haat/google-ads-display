540e6a516eeabfa2fe9a7f962af3f763131d407e
Jaynob <jynobpapiya12@gmail.com>
2023-06-14 23:26:21 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AdPreview from "./components/AdPrivew.vue";
+
+export default {
+  components: {
+    'adpreview': AdPreview,
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>Google </h1>
+
+  </div>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
